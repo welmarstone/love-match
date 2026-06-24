c237fa669fc9667c764a8d4eba9774e7c5fe3a6d
E666R <elmarhatam@gmail.com>
2026-06-25 00:48:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true,
       },
-      '/ws': {
-        target: 'ws://localhost:5000',
+      "/ws": {
+        target: "ws://localhost:5000",
         ws: true,
       },
     },
